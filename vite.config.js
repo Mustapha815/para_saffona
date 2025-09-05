a4ee3cc8c7524fb25595f0abd29aa2f932fbd71c
Your Namel <you@example.com>
2025-09-05 22:25:04 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
       '/api': {
         target: `${import.meta.env.VITE_API_BASE_URL}`, // Laravel backend
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '/api'),
-      },
     },
   },
 });
