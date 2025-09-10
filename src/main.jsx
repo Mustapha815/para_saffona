3a9148654584d945585bdc8ec6aa4b870106ba12
Ayoub El mssellek <ayoubmssallak7@gmail.com>
2025-09-10 17:27:35 +0200

    Add sitemap.xml and index.html

--- src/main.jsx
+++ src/main.jsx
@@ -7,20 +7,20 @@ import App from './App.jsx';
 import './index.css';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
+  <StrictMode>
   <BrowserRouter basename="/">
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <App />
-      
+        <App />   
       </QueryClientProvider>
     </Provider>
   </BrowserRouter>
+  </StrictMode>
 );
 
  
