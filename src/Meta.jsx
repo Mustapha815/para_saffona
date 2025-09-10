c221cba2cac4c0cfdf7c6b9c0b564be58cacaa85
Ayoub El mssellek <ayoubmssallak7@gmail.com>
2025-09-10 17:36:08 +0200

    Add sitemap.xml and index.html and Helmet

---
+++ src/Meta.jsx
@@ -0,0 +1,22 @@
+import { Helmet } from 'react-helmet';
+
+const Meta = ({ title, description, image, url }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      {image && <meta property="og:image" content={image} />}
+      {url && <meta property="og:url" content={url} />}
+      <meta property="og:type" content="website" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      {image && <meta name="twitter:image" content={image} />}
+    </Helmet>
+  );
+};
+
+export default Meta;
--- src/components/Client/pages/Home.jsx
+++ src/components/Client/pages/Home.jsx
@@ -1,21 +1,49 @@
-import React from 'react'
-import Header from '../Layout/ClientHeader'
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import Header from '../Layout/ClientHeader';
 import HeroSection from '../Layout/HeroSection';
 import ProductListing from '../Layout/ProductListing';
 import ClientFooter from '../Layout/ClientFooter';
-import  LocalisationSection from '../Layout/LocalisationSection';
+import LocalisationSection from '../Layout/LocalisationSection';
 import BrandSection from '../Layout/Brands';
 
- const Home = () => {
+const Home = () => {
   return (
     <div>
+      {/* ✅ Meta tags for SEO and Social Sharing */}
+      <Helmet>
+        <title>Parasaffona | الصفحة الرئيسية</title>
+        <meta
+          name="description"
+          content="أفضل منصة صيدليات وبارافارماسية في الداخلة، تقدم منتجات العناية بالبشرة، المكملات الغذائية، منتجات الأطفال والمزيد."
+        />
+        <meta property="og:title" content="Parasaffona | أفضل منصة بارافارماسية في الداخلة" />
+        <meta
+          property="og:description"
+          content="اكتشف أفضل منتجات الصيدليات في الداخلة: العناية بالبشرة، المكملات الغذائية، منتجات الأطفال والمزيد."
+        />
+        <meta property="og:image" content="https://parasaffona.com/logo.jpg" />
+        <meta property="og:url" content="https://parasaffona.com/" />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Parasaffona | الصفحة الرئيسية" />
+        <meta
+          name="twitter:description"
+          content="أفضل منصة صيدليات وبارافارماسية في الداخلة، منتجات العناية بالبشرة، المكملات الغذائية، منتجات الأطفال والمزيد."
+        />
+        <meta name="twitter:image" content="https://parasaffona.com/logo.jpg" />
+      </Helmet>
+
+      {/* الصفحة نفسها */}
       <Header />
       <HeroSection />
-      <BrandSection/>
-    < ProductListing/>
-      <LocalisationSection/>
-      <ClientFooter/>
+      <BrandSection />
+      <ProductListing />
+      <LocalisationSection />
+      <ClientFooter />
     </div>
-  )
-}
-export default Home
+  );
+};
+
+export default Home;
