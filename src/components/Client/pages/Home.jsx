81f94c20231c739bbbff0c08091c3a158e56e9c1
Ayoub El mssellek <ayoubmssallak7@gmail.com>
2025-09-10 17:40:50 +0200

    Add sitemap.xml and index.html and Helmet

--- src/components/Client/pages/Home.jsx
+++ src/components/Client/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import Meta from '../../../Meta'; // path صحيح حسب المكان ديال الملف
 import Header from '../Layout/ClientHeader';
 import HeroSection from '../Layout/HeroSection';
 import ProductListing from '../Layout/ProductListing';
@@ -10,30 +10,13 @@ import BrandSection from '../Layout/Brands';
 const Home = () => {
   return (
     <div>
-      {/* ✅ Meta tags for SEO and Social Sharing */}
-      <Helmet>
-        <title>Parasaffona | الصفحة الرئيسية</title>
-        <meta
-          name="description"
-          content="أفضل منصة صيدليات وبارافارماسية في الداخلة، تقدم منتجات العناية بالبشرة، المكملات الغذائية، منتجات الأطفال والمزيد."
-        />
-        <meta property="og:title" content="Parasaffona | أفضل منصة بارافارماسية في الداخلة" />
-        <meta
-          property="og:description"
-          content="اكتشف أفضل منتجات الصيدليات في الداخلة: العناية بالبشرة، المكملات الغذائية، منتجات الأطفال والمزيد."
-        />
-        <meta property="og:image" content="https://parasaffona.com/logo.jpg" />
-        <meta property="og:url" content="https://parasaffona.com/" />
-        <meta property="og:type" content="website" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Parasaffona | الصفحة الرئيسية" />
-        <meta
-          name="twitter:description"
-          content="أفضل منصة صيدليات وبارافارماسية في الداخلة، منتجات العناية بالبشرة، المكملات الغذائية، منتجات الأطفال والمزيد."
-        />
-        <meta name="twitter:image" content="https://parasaffona.com/logo.jpg" />
-      </Helmet>
+      {/* ✅ Meta Component */}
+      <Meta
+        title="Parasaffona | الصفحة الرئيسية"
+        description="أفضل منصة صيدليات وبارافارماسية في الداخلة، تقدم منتجات العناية بالبشرة، المكملات الغذائية، منتجات الأطفال والمزيد."
+        image="https://parasaffona.com/logo.jpg"
+        url="https://parasaffona.com/"
+      />
 
       {/* الصفحة نفسها */}
       <Header />
