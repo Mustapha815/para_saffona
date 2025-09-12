62b7c674e3856e76eb2827b6f478346ca67f8d15
ayoubelmssellek <mssallak8@gmail.com>
2025-09-13 00:57:02 +0200

    Update Brands.jsx

--- src/components/Client/Layout/Brands.jsx
+++ src/components/Client/Layout/Brands.jsx
@@ -153,7 +153,7 @@ const maxScroll = container.scrollWidth / 2; // half = original length
         <img 
           src={`${import.meta.env.VITE_IMG_BASE_URL}/${brand.image}`} 
           alt={brand.name} 
-          className="h-16 w-full md:h-20 object-cover  rounded-tl-xl rounded-br-xl" 
+          className="h-16 w-full md:h-20 object-fit  rounded-tl-xl rounded-br-xl" 
           loading='lazy'
         />
       </div>
